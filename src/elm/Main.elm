8fc0d96f74b6f543feec7ea15a3b144f0464ed06
Joshua Clayton <joshua.clayton@gmail.com>
2016-12-08 11:43:23 -0500

    Initial data model

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (..)
 
+import Dict exposing (Dict)
 import Html exposing (..)
 
 
-main : Program Never Int Msg
+main : Program Never Model Msg
 main =
     Html.program
         { init = ( model, Cmd.none )
@@ -17,13 +18,64 @@ main =
 -- MODEL
 
 
+type alias Prices =
+    Dict Drug Dollar
+
+
+type Dollar
+    = Dollar Int
+
+
+type Location
+    = Manhattan
+    | Bronx
+    | Ghetto
+    | ConeyIsland
+    | CentralPark
+    | Brooklyn
+
+
+type Drug
+    = Cocaine
+    | Heroin
+    | Acid
+    | Weed
+    | Speed
+    | Ludes
+
+
+type alias Stash =
+    Dict Drug Int
+
+
+type alias Inventory =
+    { drugs : Stash
+    , maxHolding : Int
+    , guns : Int
+    }
+
+
 type alias Model =
-    Int
+    { currentLocation : Location
+    , currentPrices : Prices
+    , cashOnHand : Dollar
+    , inventory : Inventory
+    , stash : Stash
+    , debt : Dollar
+    , bankAccountBalance : Dollar
+    }
 
 
-model : number
+model : Model
 model =
-    0
+    Model
+        Manhattan
+        Dict.empty
+        (Dollar 2000)
+        (Inventory Dict.empty 100 0)
+        Dict.empty
+        (Dollar 5500)
+        (Dollar 0)
 
 
 type Msg
